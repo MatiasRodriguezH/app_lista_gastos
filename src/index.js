cf934376b2a91f95abc5a72b55dd69ae39571fe7
Matias <ma.antoniorodriguez@gmail.com>
2025-05-30 16:02:02 -0400

    Iniciamos chat

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import WebFont from 'webfontloader';
+import Contenedor from './elementos/Contenedor';
+
+
+WebFont.load({
+  google: {
+    families: ['Work Sans:400,500,700', 'sans-serif']
+  }
+});
+
+
+const Index = () => {
+  return (
+      <App/>
+  );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<React.StrictMode><Index /></React.StrictMode>);
