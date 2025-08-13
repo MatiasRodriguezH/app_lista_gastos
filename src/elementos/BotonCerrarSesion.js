8036ff3592b38377e12f11e7ed9424c9364a842b
Matias <ma.antoniorodriguez@gmail.com>
2025-08-13 12:00:07 -0400

    Eliminamos unos console.log()

--- src/elementos/BotonCerrarSesion.js
+++ src/elementos/BotonCerrarSesion.js
@@ -1,27 +1,27 @@
 import React from "react";
-import {ReactComponent as IconoCerrarSesion} from "./../imagenes/log-out.svg"
+import { ReactComponent as IconoCerrarSesion } from "./../imagenes/log-out.svg";
 import Boton from "./Boton";
-import {auth} from "./../firebase/firebaseConfig";
+import { auth } from "./../firebase/firebaseConfig";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
 const BotonCerrarSesion = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const cerrarSesion = async() => {
-        try {
-            await signOut(auth);
-            navigate("/iniciar-sesion");
-        } catch(error){
-            console.log(error);
-        }
+  const cerrarSesion = async () => {
+    try {
+      await signOut(auth);
+      navigate("/iniciar-sesion");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <Boton iconoGrande as="button" onClick={cerrarSesion}>
-            <IconoCerrarSesion/>
-        </Boton>
-    );
-}
- 
+  return (
+    <Boton iconoGrande as="button" onClick={cerrarSesion}>
+      <IconoCerrarSesion />
+    </Boton>
+  );
+};
+
 export default BotonCerrarSesion;
